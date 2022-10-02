92742a0debc2512a040dc7cf91e3aad90d546e5b
Jamie Poulson <911us@duck.com>
2022-10-02 07:52:33 +0000

    BTC.py

---
+++ btc.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+response = requests.get("https://coinmarketcap.com/")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text,
+                      features="html.parser")
+    soup_list = soup.find_all("a", {"href": "/currencies/ethereum/markets/"})
+    res = soup_list[0].find("span")
+    print(res.text)
